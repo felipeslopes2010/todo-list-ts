b95d90f575140e7b1da36a40fb3ee2560d8f577d
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-11-16 19:35:58 -0300

    Add: Remove and Add Tasks Funcionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,111 @@
-import { Header } from './components/Header'
-import { Task } from './components/Task'
-import { PlusCircle } from "phosphor-react"
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'; 
+
+import { Header } from './components/Header';
+import { Task } from './components/Task';
+import { PlusCircle } from "phosphor-react";
 
 import styles from './App.module.css';
 import './global.css';
 
+export interface TaskInterface {
+  id: string;
+  content: string;
+  completed: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<TaskInterface[]>([]);
+  const [newTaskContent, setNewTaskContent] = useState('');
+
+  const createTasksCount = tasks.length;
+  const completedTasksCount = tasks.filter(task => task.completed).length;
+
+  function handleNewTask() {
+    const newTask = {
+      id: uuidv4(),
+      content: newTaskContent,
+      completed: false,
+    };
+
+    if(newTask.content.length === 0) {
+      return alert("Informar nome da nova tarefa!");
+    }
+
+    setTasks([...tasks, newTask]);
+
+    setNewTaskContent('');
+  }
+
+  function handleCompleteTask(id: string) {
+    const updatedTasks = tasks.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+
+    setTasks(updatedTasks);
+  }
+
+  function handleRemoveTask(taskToRemove: string) {
+    setTasks(tasks.filter(task => task.id !== taskToRemove));
+  }
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("@todo-list:tasks");
+
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (tasks.length > 0) {
+      localStorage.setItem("@todo-list:tasks", JSON.stringify(tasks));
+    }
+  }, [tasks]);
+  
   return (
     <>
       <Header />
       <div className={styles.container}>
-
-        <div className={styles.searchWrapper}>
+        <div className={styles.inputWrapper}>
           <input
             type="text"
+            value={newTaskContent}
+            onChange={e => setNewTaskContent(e.target.value)}
             placeholder="Adicione uma nova tarefa"
           />
-          <button
-            type="button"
-          >
+          <button type="button" onClick={handleNewTask}>
             Criar
-            <PlusCircle size={16} />
+            <PlusCircle size={22} />
           </button>
         </div>
 
         <div className={styles.taskListHeader}>
           <div className={styles.createdTasks}>
             <strong>Tarefas criadas</strong>
-            <span>0</span>
+            <span>{createTasksCount}</span>
           </div>
           <div className={styles.doneTasks}>
             <strong>Concluídas</strong>
-            <span>0</span>
+            <span>{completedTasksCount}</span>
           </div>
         </div>
 
         <div className={styles.taskList}>
-          <Task />
-          <Task />
-          <Task />
+          {tasks.map((task: TaskInterface) => (
+            <Task
+              key={task.id}
+              id={task.id}
+              content={task.content}
+              completed={task.completed}
+              onComplete={handleCompleteTask}
+              onRemove={handleRemoveTask}
+            />
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,19 @@
 import { Trash } from "phosphor-react"
 
 import styles from "./Task.module.css";
+import { TaskInterface } from "../App";
 
-export function Task() {
+export function Task({ id, content, completed = false, onComplete, onRemove }: TaskInterface & { onComplete: (id: string) => void, onRemove: (id: string) => void }) {
     return (
         <div className={styles.container}>
-            <input type="checkbox" className={styles.checkbox} />
-            <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-            <Trash />
+            <input
+                type="checkbox"
+                className={styles.checkbox}
+                checked={completed}
+                onChange={() => onComplete(id)}
+            />
+            { completed ? <p className={styles.taskCompleted}>{content}</p> : <p className={styles.taskIncompleted}>{content}</p>}
+            <Trash onClick={() => onRemove(id)} />
         </div>
     )
 }
